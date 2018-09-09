0283d3e10828a721897958d13e15f39172c8afdb
krishnakushare <krishkusharekk@gmail.com>
2018-09-09 15:08:06 +0530

    transnav

--- apurvaEngg/src/app/app.module.ts
+++ apurvaEngg/src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './main/home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
-  ],
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent  ],
   imports: [
     BrowserModule
   ],
